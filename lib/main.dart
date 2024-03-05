227bcaaf1dc8af9a5d152a029e28ec306563ec1c
Utkarsh Mishra <utkarsh971762@gmail.com>
2024-03-05 22:50:49 +0530

    added images

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practice_application/addimages.dart';
 import 'package:practice_application/buttons.dart';
 
 void main() {
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       // home: LearnContainer(),
-      home: MyButtons(),
+      // home: MyButtons(),
+      home: AddImages(),
     );
   }
 }
