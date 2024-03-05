1b354fce62aa4b2a869dd29121014dda374f1b38
Utkarsh Mishra <utkarsh971762@gmail.com>
2024-03-05 21:24:42 +0530

    Edited Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(LearnContainer());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LearnContainer(),
+    );
+  }
 }
 
 class LearnContainer extends StatelessWidget {
@@ -9,24 +21,27 @@ class LearnContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          body: Center(
-            child: Container(
-              height: 200,
-              width: 200,
-              child: Center(
-                child: Text(
-                  'this is a container',
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
+    return Scaffold(
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.lightBlue,
+            child: const Center(
+              child: Text(
+                'this is a container',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    backgroundColor: Colors.white),
               ),
-              color: Colors.lightBlue,
             ),
           ),
-          appBar: AppBar(
-            title: Text('Container'),
-          )),
-    );
+        ),
+        appBar: AppBar(
+          title: Text('Container'),
+        ));
   }
 }
