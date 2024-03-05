8aafaac7df4ca31d972e646b2c3c9327690f12ee
Utkarsh Mishra <utkarsh971762@gmail.com>
2024-03-05 20:27:57 +0530

    Added Container

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,16 @@ class LearnContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          body: Container(
+            child: Text(
+              'this is a container',
+              style: TextStyle(color: Colors.black),
+            ),
+            color: Colors.lightBlue,
+          ),
           appBar: AppBar(
-        title: Text('Container'),
-      )),
+            title: Text('Container'),
+          )),
     );
   }
 }
